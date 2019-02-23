72e25f6e3a49f2815e3a6a0c3d0b0d7bb947d9da
chitabathina-kickstart <chitabathina.kickstart@gmail.com>
2019-02-22 23:22:36 -0800

    adding the basic structure of the application

---
+++ src/main/java/com/api/kickstart/kickstart/KickstartApplication.java
@@ -0,0 +1,14 @@
+package com.api.kickstart.kickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickstartApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/api/kickstart/kickstart/KickstartApplicationTests.java
@@ -0,0 +1,17 @@
+package com.api.kickstart.kickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KickstartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
